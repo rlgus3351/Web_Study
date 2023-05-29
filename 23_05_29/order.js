1f0cd1ee913391ea8134443e255f5a7c75d9118d
rlgus3351 <rlgus3351@naver.com>
2023-05-29 23:54:04 +0900

    23/05/29

---
+++ 23_05_29/order.js
@@ -0,0 +1,32 @@
+const Orders=[
+    {
+        productName : "test1",
+        productNumber : "0001",
+        paymentStauts: "Due",
+        shipping : 'Pending'
+    },
+    {
+        productName : "test2",
+        productNumber : "0002",
+        paymentStauts: "Due",
+        shipping : 'Declined'
+    },
+    {
+        productName : "test3",
+        productNumber : "0003",
+        paymentStauts: "Due",
+        shipping : 'Pending'
+    },
+    {
+        productName : "test4",
+        productNumber : "0004",
+        paymentStauts: "Due",
+        shipping : 'Delivered'
+    },
+    {
+        productName : "test5",
+        productNumber : "0005",
+        paymentStauts: "Due",
+        shipping : 'Delivered'
+    }
+]
